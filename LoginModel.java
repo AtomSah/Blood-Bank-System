ddb7929d9ce4ab0500a1d20ecfcde3ea1cc762fe
Karanbohara01 <125890403+Karanbohara01@users.noreply.github.com>
2023-08-11 19:50:54 +0545

    Add files via upload
    login model

---
+++ LoginModel.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author shaha
+ */
+public class LoginModel {
+    
+    String Email,Password;
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
+    public LoginModel ( String Email,String Password)
+    {
+        this.Email = Email;
+        this.Password = Password;
+    }
+    
+}
